b3833d1e5a91ff7fa5383582972e29a9ad3f597e
nightzillla <81122802+nightzillla@users.noreply.github.com>
2022-11-14 17:45:52 -0600

    updated followers, following, public repos and gists in react

--- src/pages/User.jsx
+++ src/pages/User.jsx
@@ -112,6 +112,52 @@ function User() {
           </div>
         </div>
       </div>
+      <div className="w-full py-5 mb-6 rounded-lg shadow-md bg-base-100 stats">
+        <div className='stat'>
+          <div className="stat-figure text-secondary">
+            <FaUsers className='text-3xl md:text-5xl'/>
+          </div>
+          <div className="stat-title pr-5">
+            Followers
+          </div>
+          <div className="stat-value pr-5 text-3xl md:text-4xl">
+            {followers}
+          </div>
+        </div> {/* END OF FOLLOWERS */}
+        <div className='stat'>
+          <div className="stat-figure text-secondary">
+            <FaUsers className='text-3xl md:text-5xl'/>
+          </div>
+          <div className="stat-title pr-5">
+            Following
+          </div>
+          <div className="stat-value pr-5 text-3xl md:text-4xl">
+            {following}
+          </div>
+        </div>{/* END OF FOLLOWING */}
+        <div className='stat'>
+          <div className="stat-figure text-secondary">
+            <FaCodepen className='text-3xl md:text-5xl'/>
+          </div>
+          <div className="stat-title pr-5">
+            Public Repos
+          </div>
+          <div className="stat-value pr-5 text-3xl md:text-4xl">
+            {public_repos}
+          </div>
+        </div>{/* END OF PUBLIC REPOS */}
+        <div className='stat'>
+          <div className="stat-figure text-secondary">
+            <FaStore className='text-3xl md:text-5xl'/>
+          </div>
+          <div className="stat-title pr-5">
+            Public Gists
+          </div>
+          <div className="stat-value pr-5 text-3xl md:text-4xl">
+            {public_gists}
+          </div>
+        </div>{/* END OF PUBLIC GISTS */}
+        </div>
     </div>
     </>
   )
